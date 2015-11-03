f2d7fcebe6acb8322b35b4c2072af169ad367436
HuynhThanh <HuynhThanh@ThanhHuynh>
2015-11-03 13:16:54 +0700

    test thử

--- src/jWhiteBoard/JWhiteBoard.java
+++ src/jWhiteBoard/JWhiteBoard.java
@@ -25,6 +25,7 @@ import java.util.List;
  * 
  * 
  */
+//Bai mau kiem tra
 
 public class JWhiteBoard extends ReceiverAdapter implements ActionListener, ChannelListener {
     protected String               groupName="";
